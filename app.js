50f4c0fce8ad0f271701b39052c5e09df1fe43e6
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-06-19 14:10:36 +0500

    Online-Store

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-//   .sync()
+  // .sync({ force: true }) // updating the tables
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
--- models/product.js
+++ models/product.js
@@ -5,8 +5,8 @@ const sequelize = require('../util/database');
 const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
+    autoIncrement: true, 
+    allowNull: false,    
     primaryKey: true
   },
   title: Sequelize.STRING,
