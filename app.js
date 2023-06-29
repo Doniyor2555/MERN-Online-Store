550b448e613e2596edd32470f6230dea3b67d52b
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-06-29 12:59:28 +0500

    Online-store

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('649d2bc80bfe3be7bb1c0890')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
